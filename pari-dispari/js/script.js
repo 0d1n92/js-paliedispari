9731abf6def155d5877a87b8068d871dcfc7c97a
enrico <rombaldonienrico@gmail.com>
2020-11-18 15:10:46 +0100

    algorithm even completed

--- pari-dispari/js/script.js
+++ pari-dispari/js/script.js
@@ -5,17 +5,20 @@
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione).
 // Dichiariamo chi ha vinto.
 var evenOrUneven=prompt("scommettiamo! secondo te la somma verrà pari o dispari?");
+console.log("scommetti sul:", evenOrUneven);
 var number=parseInt(prompt("inserisci un numero da uno a 5 da sommare con quello del computer"));
 var numberBot= Math.floor(Math.random()*5)+1;
 console.log("il numero del pc: ", numberBot);
 number+=numberBot;
 console.log("la somma dei due numeri è:", number)
-var result=false;
-
+var even="dispari"
 if (fxEven(number)){
-  console.log("pari");
-
+  var even="pari";
 }
-else console.log("dispari")
-
+var message="perso";
+if(fxComparation(evenOrUneven,even)){
+  message="hai vinto"
+}
+console.log(message)
+function fxComparation(arg1, arg2) {return arg1==arg2;}
 function fxEven(arg1){ return number%2==0;}
